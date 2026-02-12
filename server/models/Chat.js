e4cca8b165d4cb843fa218da449d72874cf05e0e
Hitesh Panchal <hitesh.panchal@carer.ai>
2026-02-12 21:18:42 +0530

    inital commit

---
+++ server/models/Chat.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+  {
+    userId: { type: String, ref: "User", required: true },
+    userName: { type: String, required: true },
+    name: { type: String, required: true },
+    messages: [
+      {
+        isImage: { type: Boolean, required: true },
+        isPublished: { type: Boolean, default: false },
+        role: { type: String, default: true },
+        content: { type: String, default: true },
+        timestamp: { type: Number, default: true },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const Chat = mongoose.model("Chat", ChatSchema);
+
+export default Chat;
